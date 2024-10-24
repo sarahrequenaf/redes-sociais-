a981ddc82c4204b320d42ebf359cbd78e9f3955e
sarahrequenaf <128055877+sarahrequenaf@users.noreply.github.com>
2024-10-24 13:38:35 +0000

    kkk

--- graficos/ informacoesGlobais.js
+++ graficos/ informacoesGlobais.js
@@ -1,1 +1,7 @@
-asyn
+async function visualizarInformacoesGlobais() { 
+    const ress = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = document.createElement('p') 
+    paragrafo.classList.add('graficos-container tsxto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em algumarede social e passam em média ${dados.tempo_medio} horas conectadas.` console.log(paragrafo) } 
+    
